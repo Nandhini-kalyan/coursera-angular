f22e0e63cb870e229e0623442083609fa639b009
nandhini <knandhini867@gmail.com>
2020-07-13 21:21:15 +0530

    second upload after inject

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,6 +4,7 @@
     .module("lunchCheck", [])
 
     .controller("checkController", checkController);
+  checkController.$injector = ["$scope"];
 
   function checkController($scope) {
     $scope.listOfItems = "";
